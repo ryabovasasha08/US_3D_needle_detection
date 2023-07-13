49730cb61db3b8599d68d23d5a353947ae0b9104
Oleksandra <oleksandra.riabova@tuhh.de>
2023-07-13 14:10:49 +0200

    Group label-related functions in labels_utils Try to use miscnn (beginning)

--- utils/ImageDataset.py
+++ utils/ImageDataset.py
@@ -10,6 +10,38 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 from skimage.transform import resize
 
+from utils.type_reader import get_image_array
+from skimage.transform import resize
+
+# since resolution is 3px=mm and we have +-1mm error, default mask is 3px radius
+# 135 is initial image size so also the default ResizeTo
+def getDatapointResized(filename, frame_num, labels, mask_diam=3, resizeTo = 135): 
+    # create frame image based on frame number and filename
+    input_image = get_image_array(filename)[:, :, :, frame_num]
+
+    # create mask for the frame image
+    mask_image = np.zeros((input_image.shape))
+    mask_image[
+        np.around(labels[0]-mask_diam/2).astype(int):np.around(labels[0]+mask_diam/2).astype(int), 
+        np.around(labels[1]-mask_diam/2).astype(int):np.around(labels[1]+mask_diam/2).astype(int), 
+        np.around(labels[2]-mask_diam/2).astype(int):np.around(labels[2]+mask_diam/2).astype(int)
+    ] = 1
+    
+    us_tip_coords = np.around(labels).astype(int)
+    
+    #resize everything to 128*128*128 and normalize
+    ratio = 128/135
+    us_tip_coords_resized = np.around(us_tip_coords*ratio).astype(int)
+    
+    input_image = resize(input_image, (128, 128, 128))[np.newaxis, :, :, :]
+    mean = np.mean(input_image)
+    std = np.std(input_image)
+    input_image = (input_image - mean) / std
+    
+    mask_image = resize(mask_image, (128, 128, 128))[np.newaxis, :, :, :]
+
+    return input_image, mask_image, us_tip_coords_resized
+
 class ImageDataset(Dataset):
     """US Images with a tip needle dataset."""
 
@@ -30,32 +62,10 @@ class ImageDataset(Dataset):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
-        # create frame image based on frame number and filename
-        input_image = get_image_array(self.X[idx, 0])[:, :, :, int(float(self.X[idx, 1]))]
+        input_img, mask_img, us_tip_coords_resized = \
+            getDatapointResized(self.X[idx, 0], self.X[idx, 1], self.y[idx], mask_diam=3, resizeTo=128)
 
-        # create mask for the frame image
-        mask_diam = 3 # since resolution is 3px=mm and we have +-1mm error, mask should be 3px radius
-        mask_image = np.zeros((input_image.shape))
-        mask_image[
-            np.around(self.y[idx][0]-mask_diam/2).astype(int):np.around(self.y[idx][0]+mask_diam/2).astype(int), 
-            np.around(self.y[idx][1]-mask_diam/2).astype(int):np.around(self.y[idx][1]+mask_diam/2).astype(int), 
-            np.around(self.y[idx][2]-mask_diam/2).astype(int):np.around(self.y[idx][2]+mask_diam/2).astype(int)
-        ] = 1
-        
-        us_tip_coords = np.around(self.y[idx]).astype(int)
-        
-        #resize everything to 128*128*128 and normalize
-        ratio = 128/135
-        us_tip_coords_resized = np.around(us_tip_coords*ratio).astype(int)
-        
-        input_image = resize(input_image, (128, 128, 128))[np.newaxis, :, :, :]
-        mean = np.mean(input_image)
-        std = np.std(input_image)
-        input_image = (input_image - mean) / std
-        
-        mask_image = resize(mask_image, (128, 128, 128))[np.newaxis, :, :, :]
-
-        sample = {'image': input_image, 'mask': mask_image, 'label': us_tip_coords_resized}
+        sample = {'image': input_img, 'mask': mask_img, 'label': us_tip_coords_resized}
 
         if self.transform:
             sample = self.transform(sample)
---
+++ utils/labels_utils.py
@@ -0,0 +1,88 @@
+import numpy as np
+from os import listdir
+import os
+from os.path import isfile, join
+
+# import all files from necessary directory
+def get_all_files_mhd(dir = "/data/Riabova/train3"):
+    all_files = [join(dir, f) for f in listdir(dir)]
+    all_files_mhd = [string for string in all_files if string.endswith("mhd")]
+    return all_files_mhd
+
+def get_labels(f, info):
+    
+    labels = np.zeros((info['Dimensions'][3], 3))
+        
+    for frame in range(0, info['Dimensions'][3]):
+        if frame > 9:
+            info_frame_timestamp_field = "Seq_Frame00" + str(frame) + "_Timestamp"
+        else:
+            info_frame_timestamp_field = "Seq_Frame000" + str(frame) + "_Timestamp"
+            
+        frame_timestamp = float(info[info_frame_timestamp_field])
+        
+        # filename in format 'x_y_z_alpha_beta_gamma_velocity_axis_timestamp
+        filename = f.split("/")[-1].split(".")[0] # 0_22_5_0_0_0_3_1_1686308127391259
+        frame_timestamp_micros = frame_timestamp*1000000 #in microseconds, correct
+        
+         # extract params from filename
+        filename_params = filename.split('_')
+        start_timestamp_micros = float(filename_params[-1]) # in micros
+        axis = int(filename_params[-2])
+        velocity = int(filename_params[-3])  # in mm/s
+
+        # get tip coords in hexapode system
+        dt = frame_timestamp_micros - start_timestamp_micros  # in micros
+        distance = velocity * dt / 1000000  # in mm
+        hex_coords = [0, 0, 0]
+        for i in range(3):
+            hex_coords[i] = float(filename_params[i])
+            if axis == i+1:
+                hex_coords[i] += distance
+                        
+        # get tip coords in US system
+        labels[frame, :] = transformToUS(hex_coords)
+        
+    return labels
+
+
+''' 
+Example usage:
+    coords_1 = [x1, x2, x3]  # Replace x1, x2, x3 with the actual values
+    method = "hex2us"  # Or "us2hex"
+    isShifted = True  # Or False
+    coords_2 = transformTo(coords_1, method, isShifted) 
+
+'''
+def transformTo(coords_1, method, isShifted):
+    # Transformation matrices
+    T_needle_tip = np.array([[-0.1601, 3.1823, 0.1383, 13.8778],
+                            [0.0178, 0.0806, -3.3695, 112.5630],
+                            [-2.4504, -0.1146, -0.3139, 110.3655],
+                            [-0.0000, -0.0000, -0.0000, 1.0000]])
+
+    T_needle_tip_shifted = np.array([[-0.1601, 3.1823, 0.1383, 20.8778],
+                                     [0.0178, 0.0806, -3.3695, 109.5630],
+                                     [-2.4504, -0.1146, -0.3139, 110.3655],
+                                     [-0.0000, -0.0000, -0.0000, 1.0000]])
+
+    if isShifted:
+        T = T_needle_tip_shifted
+    else:
+        T = T_needle_tip
+
+    coords_1 = np.append(coords_1, 1)  # Add 1 to coords_1
+
+    if method == "hex2us":
+        coords_2 = np.dot(T, coords_1.T)
+    else:
+        coords_2 = np.linalg.solve(T, coords_1.T)  # Equivalent to inv(T) * coords_1.T
+
+    coords_2 = coords_2.T
+    coords_2 = coords_2[:-1]
+
+    return coords_2
+
+
+def transformToUS(coords_1):
+    return transformTo(coords_1, "hex2us", True)
