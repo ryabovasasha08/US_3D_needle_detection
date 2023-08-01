57ca770287c4dd66d598cab54edfe9d42add37bb
Oleksandra <oleksandra.riabova@tuhh.de>
2023-08-01 13:53:26 +0200

    Reorder losses.py

--- utils/losses.py
+++ utils/losses.py
@@ -4,15 +4,15 @@ import torch.nn.functional as F
     
 # PyTorch
 # modified to add more weight to the input in denominator to punish big masks harder
-class IoULossModified(nn.Module):
+class IoULoss(nn.Module):
     def __init__(self, weight=None, size_average=True):
-        super(IoULossModified, self).__init__()
+        super(IoULoss, self).__init__()
 
-    def forward(self, inputs, targets, smooth=1):
+    def forward(self, inputs, targets, smooth=0.1):
         
         #comment out if your model contains a sigmoid or equivalent activation layer
-        inputs = torch.sigmoid(inputs)       
-        
+        inputs = (torch.sigmoid(inputs) > 0.5).float()
+   
         #flatten label and prediction tensors
         inputs = inputs.view(-1)
         targets = targets.view(-1)
@@ -26,6 +26,51 @@ class IoULossModified(nn.Module):
         IoU = (intersection + smooth)/(union + smooth)
         
         return 1 - IoU
+
+
+'''
+Here are some best practices for modifying the Jaccard index (Intersection over Union) to punish large mask predictions:
+
+Use log scale on mask sizes:
+Take log of mask sizes before computing IoU. This makes the metric more sensitive to differences between small masks compared to large masks.
+
+Add a mask size regularization term:
+IoU_modified = IoU - lambda * mask_size
+
+The regularization constant lambda controls how much to penalize large masks. Tune lambda based on your dataset.
+
+Power scale the intersection:
+Instead of simple intersection, take intersection^p, where p < 1. Smaller p more strongly penalizes large intersections (large masks).
+
+Only scale ground truth size:
+IoU_modified = Intersection / (gt_mask_size + Pred_mask_size - Intersection)
+'''
+class IoULossModified(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(IoULossModified, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1):
+        
+        #comment out if your model contains a sigmoid or equivalent activation layer
+        inputs = torch.sigmoid(inputs)
+        inputs = (inputs>0.5).int()
+       
+        
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        #intersection is equivalent to True Positive count
+        #union is the mutually inclusive area of all labels & predictions 
+        intersection = (inputs * targets).sum()
+        total = (inputs + targets).sum()
+        union = total - intersection 
+        
+        iou = (intersection + smooth) / (union + smooth)
+        
+        # TODO: add weighting
+        
+        return 1 - iou
     
     
 # PyTorch
@@ -38,7 +83,8 @@ class CompleteIoULoss(nn.Module):
 
     def forward(self, inputs, targets, smooth=1):
         
-        inputs = (torch.sigmoid(inputs) > 0.5).float()
+        inputs = torch.sigmoid(inputs)
+        inputs = (inputs>0.5).int()
         
         #flatten label and prediction tensors
         inputs = inputs.view(-1)
