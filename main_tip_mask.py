Edit the directory of train_test_data.hdf5 and fix calculation of precision/recall for tip mask

main_tip_mask.py
@@ -207,7 +207,7 @@ class TrainerUNET:
         save_plots(self.epochs, self.training_loss, self.validation_loss, self.tip_pixel_distances, self.pixelwise_accuracy, self.precisions, self.recalls, self.learning_rate, self.path_dir)
         print('TRAINING COMPLETE')
         
-        new_file = h5py.File(self.path_dir+'train_test_data.hdf5', 'w')
+        new_file = h5py.File(self.path_dir+'/train_test_data.hdf5', 'w')
         new_file.create_dataset("training_loss", data=self.training_loss)
         new_file.create_dataset("valid_loss", data=self.validation_loss)
         new_file.create_dataset("tr_tip_pixel_distance", data=self.tip_pixel_distances)
@@ -259,8 +259,8 @@ class TrainerUNET:
             train_losses.append(loss_value)
             pixelwise_accuracy_within_batch.append(get_pixel_accuracy_percent(out, target))
             tip_pixel_distance_within_batch.append(get_central_pixel_distance(out, labels))
-            precisions_within_batch.append(get_precision(out, labels))
-            recalls_within_batch.append(get_recall(out, labels))
+            precisions_within_batch.append(get_precision(out, target))
+            recalls_within_batch.append(get_recall(out, target))
 
             batch_iter.set_description(f'Training: (loss {loss_value:.4f})')  # update progressbar
         
@@ -336,7 +336,7 @@ class TrainerUNET:
             
                 batch_iter.set_description(f'Test: (loss {loss_value:.4f})')
                 
-        new_file = h5py.File(self.path_dir+'train_test_data.hdf5', 'a')
+        new_file = h5py.File(self.path_dir+'/train_test_data.hdf5', 'a')
         new_file.create_dataset("test_loss", data=np.mean(test_losses))
         new_file.create_dataset("test_tip_pixel_distance", data=np.mean(tip_pixel_distances))
         new_file.create_dataset("test_pixelwise_accuracy", data=np.mean(pixelwise_accuracies))